3bbf78e7f63b40e948d0542204f7c193746e30ea
Charly <carloshermiagae@gmail.com>
2024-07-10 09:38:12 -0300

    prueba

--- js/login.js
+++ js/login.js
@@ -1,11 +1,34 @@
-if (typeof alert != "undefined") {
-    Swal.fire({
-        title: '<%= alertTitle %>',
-        text: '<%= alertMessage %>',
-        icon: '<%= alertIcon %>',
-        showConfirmButton: ' ',
-        timer: 'timer'
+const mensajeError = document.getElementsByClassName("error")[0]
+
+document.getElementById("login-form").addEventListener("submit",async (e)=>{
+  e.preventDefault();
+  const user = e.target.children.user.value;
+  const password = e.target.children.password.value;
+  const res = await fetch("http://localhost:4000/api/login",{
+    method:"POST",
+    headers:{
+      "Content-Type":"application/json"
+    },
+    body: JSON.stringify({
+      user,password
+    })
+  });
+  if(!res.ok) return mensajeError.classList.toggle("escondido",false);
+  const resJson = await res.json();
+  if(resJson.redirect){
+    window.location.href = resJson.redirect;
+  }
+})
+
+
+//
+Swal.fire({
+    alert: true,
+    alertTitle: "Login",
+    alertMessage: "Inicio de sesion correcto",
+    alertIcon: 'success',
+    showConfirmButton: false,
+    timer: 1500,
 }).then(() => {
-            window.location = '/<%= ruta %>'
-        })
-}
+    window.location = 'https://refilm.netlify.app/login'
+})
--- js/registration.js
+++ js/registration.js
@@ -1,37 +1,44 @@
-document.getElementById('registerForm').addEventListener('submit', async function (event) {
-    event.preventDefault();
+document.getElementById("register-form").addEventListener("submit", async (e) => {
+    e.preventDefault();
 
-    const formData = new FormData(event.target);
-    const data = Object.fromEntries(formData.entries());
+    const res = await fetch("https://refilms-be.onrender.com/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            first_name: e.target.children.firstname.value,
+            last_name: e.target.children.lastname.value,
+            email: e.target.children.email.value,
+            username: e.target.children.username.value,
+            password: e.target.children.password.value
+        })
+    });
 
-    try {
-        const response = await fetch(event.target.action, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(data)
-        });
-
-        const result = await response.json();
-        
-    } catch (error) {
-        console.error('Error:', error);
+    if (!res.ok) {
+        Swal.fire({
+            alert: true,
+            title: "Registro",
+            text: "Se produjo un error",
+            icon: 'error',
+            showConfirmButton: true,
+        })
+    } else {
+        Swal.fire({
+            alert: true,
+            title: "Registro",
+            text: "Registro Exitoso!",
+            icon: 'success',
+            showConfirmButton: false,
+            timer: 1500,
+        }).then(() => {
+            window.location = 'https://refilm.netlify.app/login'
+        })
     }
-});
 
-if (response.ok) {
-    Swal.fire({
-        alert: true,
-        alertTitle: "Registro",
-        alertMessage: "Registro Exitoso!",
-        alertIcon: 'success',
-        showConfirmButton: false,
-        timer: 1500,
-    }).then(() => {
-        window.location = 'https://refilm.netlify.app/login>'
-    })
-} else {
-    alert('Error: ' + result.message);
-    console.log(result);
-}
+    //if (!res.ok) return mensajeError.classList.toggle("escondido", false);
+    //const resJson = await res.json();
+    //if (resJson.redirect) {
+    //    window.location.href = resJson.redirect;
+    //}
+})
